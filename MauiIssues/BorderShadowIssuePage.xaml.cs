b920a21211bb1480cea92e514482e8ad272acb38
albilaga <albilaga@pm.me>
2024-06-13 19:23:58 +0700

    Initial commit

---
+++ MauiIssues/BorderShadowIssuePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiIssues;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        CounterBtn.Text = count == 1 ? $"Clicked {count} time" : $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+
+        FirstBorder.IsVisible = !FirstBorder.IsVisible;
+        SecondBorder.IsVisible = !SecondBorder.IsVisible;
+    }
+}
