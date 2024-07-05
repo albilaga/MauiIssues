a1558ce3606afebd912f74ccb17b7ed74018d0b5
albilaga <albilaga@pm.me>
2024-07-05 12:01:48 +0700

    Add workaround

--- MauiIssues/GridStarRowPage.xaml.cs
+++ MauiIssues/GridStarRowPage.xaml.cs
@@ -2,6 +2,8 @@ namespace MauiIssues
 {
     public partial class GridStarRowPage : ContentPage
     {
+        private double _previousWidth, _previousHeight;
+
         public GridStarRowPage()
         {
             InitializeComponent();
@@ -9,6 +11,38 @@ namespace MauiIssues
             {
                 "Easy", "Medium", "Hard", "Expert"
             });
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (width < 1 || height < 1 || Math.Abs(width - _previousWidth) < 1 ||
+                Math.Abs(height - _previousWidth) < 1) return;
+            _previousHeight = height;
+            _previousWidth = width;
+            // Workaround(height);
+        }
+
+        private void Workaround(double height)
+        {
+            if (TitleLabel.Height + ScrollViewContent.ContentSize.Height + ContinueButton.Height > height)
+            {
+                ContainerGrid.RowDefinitions =
+                [
+                    new RowDefinition(GridLength.Auto),
+                    new RowDefinition(GridLength.Star),
+                    new RowDefinition(GridLength.Auto)
+                ];
+            }
+            else
+            {
+                ContainerGrid.RowDefinitions =
+                [
+                    new RowDefinition(GridLength.Auto),
+                    new RowDefinition(GridLength.Auto),
+                    new RowDefinition(GridLength.Auto)
+                ];
+            }
         }
     }
 }
