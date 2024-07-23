99f262a27a6d0a6ea56cc2666eeeb0ca3129089d
albilaga <albilaga@pm.me>
2024-07-23 16:26:53 +0700

    Update issues link

--- MauiIssues/ScrollViewWidth.xaml.cs
+++ MauiIssues/ScrollViewWidth.xaml.cs
@@ -1,6 +1,6 @@
 namespace MauiIssues
 {
-    // https://github.com/dotnet/maui/issues/23773
+    // https://github.com/dotnet/maui/issues/20590
     public partial class ScrollViewWidth : ContentPage
     {
         private double _previousWidth, _previousHeight;
